79e2646e3c109c013a535a558491fc818b849b2b
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-14 14:36:13 -0500

    Adding FrogJmpSolution

---
+++ CodeChallenges/FrogJmp/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeChallenges.FrogJmp
+{
+    public class Solution
+    {
+        public int solution(int X, int Y, int D)
+        {
+            if (X == Y) return 0;
+
+            var distance = Y - X;
+            var stepCount = distance / D;
+            var leftOver = distance % D;
+
+            if (leftOver > 0)
+            {
+                stepCount++;
+            }
+            return stepCount;
+        }
+    }
+}
---
+++ CodeChallengesTests/FrogJmpSolutionTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    internal class FrogJmpSolutionTest
+    {
+        [TestCase(10,85,30,3)]
+        [TestCase(10,100,30,3)]
+        [TestCase(25,85,30,2)]
+        public void AsserThatSolutionReturnsMinimalNumberOfJumpsFromXtoY(int X, int Y, int D, int expected)
+        {
+            var target = new CodeChallenges.FrogJmp.Solution();
+            var actual = target.solution(X, Y, D);
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestCase(int.MinValue,int.MaxValue,1, 100)]
+        public void AsserThatSolutionReturnsMinimalNumberOfJumpsFromXtoYinScopeOfTimeout(int X, int Y, int D, int timeout)
+        {
+            
+            var target = new CodeChallenges.FrogJmp.Solution();
+            System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+            target.solution(X, Y, D);
+            timer.Stop();
+            int milliseconds = timer.Elapsed.Milliseconds;
+            bool ExecutionTimeLessThanFiveSeconds = (milliseconds < timeout);
+            Assert.IsTrue(ExecutionTimeLessThanFiveSeconds, "Should take less than timeout");
+
+        }
+    }
+}
