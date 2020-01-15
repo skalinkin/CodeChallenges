25f5fa4cc40ae8c166507c72af2a992c1c570e94
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-15 16:31:03 -0500

    Adding MissingInteger Solution

---
+++ CodeChallenges/Codility/MissingInteger.cs
@@ -0,0 +1,51 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class MissingInteger
+    {
+        public int solution(int[] A)
+        {
+            var maxNumber = 1000000;
+            var numbers = new int[maxNumber + 1];
+
+            foreach (var element in A)
+            {
+                if (element < 1)
+                {
+                    continue;
+                }
+
+                numbers[element] = 1;
+            }
+
+
+            for (var i = 1; i <= numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
+/*This is a demo task.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+*/
---
+++ CodeChallengesTests/MissingIntegerSolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class MissingIntegerSolutionTest
+    {
+        [TestCase(new[] {1, 3, 6, 4, 1, 2}, 5)]
+        [TestCase(new[] {1, 2, 3}, 4)]
+        [TestCase(new[] {-1, -3}, 1)]
+        public void test_solution_whenGivenAnArrayOfIntegers_returnsSmallesPositiveIntergerThatIsNotInArray(int[] given,
+            int expected)
+        {
+            var target = new MissingInteger();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
