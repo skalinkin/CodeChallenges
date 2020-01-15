a71273956dd5066a433420b51affed9f7672497e
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-15 15:38:34 -0500

    Adding PermCheck

---
+++ CodeChallenges/Codility/PermCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace CodeChallenges.Codility
+{
+    public class PermCheck
+    {
+        public int solution(int[] A)
+        {
+            var existing = new bool[A.Length];
+
+            foreach (var i in A)
+            {
+                var index = i - 1;
+                if (index > A.Length - 1)
+                {
+                    return 0;
+                }
+                existing[index] = true;
+            }
+
+            if (!existing.Contains(false))
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CodeChallengesTests/PermCheckSolutionTest.cs
@@ -0,0 +1,25 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class PermCheckSolutionTest
+    {
+        [TestCase(new[] {4, 1, 3, 2}, 1)]
+        public void test_solution_receivesPrematureArray_shouldReturn1(int[] given, int expected)
+        {
+            var target = new PermCheck();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new[] {4, 1, 3}, 0)]
+        public void test_solution_receivesNotPrematureArray_shouldReturn0(int[] given, int expected)
+        {
+            var target = new PermCheck();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
