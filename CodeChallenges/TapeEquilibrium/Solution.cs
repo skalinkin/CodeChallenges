f2472a7df19d12a91ee7d92c356d98c68f97718f
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-14 22:13:25 -0500

    Adding solution for TapeEquilibrium

---
+++ CodeChallenges/TapeEquilibrium/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodeChallenges.TapeEquilibrium
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            var minimalDifference = int.MaxValue;
+            var length = A.Length;
+            var leftSplit = 0;
+            var rightSplit = 0;
+
+            for (var i = 0; i < length ; i++) rightSplit += A[i];
+
+            for (var index = 0; index < A.Length - 1; index++)
+            {
+                var element = A[index];
+                leftSplit += element;
+                rightSplit -= element;
+                var diff = leftSplit - rightSplit;
+                var absDiff = Math.Abs(diff);
+
+                if (minimalDifference > absDiff)
+                {
+                    minimalDifference = absDiff;
+                }
+            }
+
+            return minimalDifference;
+        }
+    }
+}
---
+++ CodeChallengesTests/TapeEquilibriumSolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class TapeEquilibriumSolutionTest
+    {
+        [TestCase(new[]{3,1,2,4,3},1)]
+        [TestCase(new[]{1,1},0)]
+        [TestCase(new[]{-1000,1000},2000)]
+        [TestCase(new[]{1000,-1000},2000)]
+        public void AssertSolutionReturnsTheMinimalDifferenceThatCanBeAchieved(int[] given, int expected)
+        {
+            var target = new CodeChallenges.TapeEquilibrium.Solution();
+            int actual = target.solution(given);
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
