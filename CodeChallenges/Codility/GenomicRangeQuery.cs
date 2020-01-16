bf59a1a54eacf479bfbd22ea5f435791d04ca29a
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-16 15:50:38 -0500

    adding progress on GenomicRangeQuery

---
+++ CodeChallenges/Codility/GenomicRangeQuery.cs
@@ -0,0 +1,68 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class GenomicRangeQuery
+    {
+        public int[] solution(string S, int[] P, int[] Q)
+        {
+            var answers = new int [P.Length];
+            var A = new int[S.Length];
+            var C = new int[S.Length];
+            var G = new int[S.Length];
+            var T = new int[S.Length];
+
+            for (var i = 0; i < S.Length; i++)
+            {
+                var nucleotid = S[i];
+                WriteLastSeenNucleotid(A, nucleotid, i, 'A');
+                WriteLastSeenNucleotid(C, nucleotid, i, 'C');
+                WriteLastSeenNucleotid(G, nucleotid, i, 'G');
+                WriteLastSeenNucleotid(T, nucleotid, i, 'T');
+            }
+
+            for (var i = 0; i < P.Length; i++)
+            {
+                if (CheckLastSeen(P, Q, A, i, answers, 1))
+                {
+                }
+                else if (CheckLastSeen(P, Q, C, i, answers, 2))
+                {
+                }
+                else if (CheckLastSeen(P, Q, G, i, answers, 3))
+                {
+                }
+                else
+                {
+                    CheckLastSeen(P, Q, T, i, answers, 4);
+                }
+            }
+
+            return answers;
+        }
+
+        private static bool CheckLastSeen(int[] P, int[] Q, int[] lastSeen, int i, int[] answers, int factor)
+        {
+            if (lastSeen[Q[i] + 1] >= P[i])
+            {
+                answers[i] = factor;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void WriteLastSeenNucleotid(int[] lastSeen, char nucleotid, int i1, char dna)
+        {
+            if (nucleotid == dna)
+            {
+                lastSeen[i1] = i1;
+            }
+            else
+            {
+                if (i1 != 0)
+                {
+                    lastSeen[i1] = lastSeen[i1 - 1];
+                }
+            }
+        }
+    }
+}
---
+++ CodeChallengesTests/GenomicRangeQuerySolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class GenomicRangeQuerySolutionTest
+    {
+        [TestCase("CAGCCTA",new[]{2,5,0}, new[]{4,5,6},new[]{2,4,1})]
+        [TestCase("C",new[]{0}, new[]{0},new[]{2})]
+        public void test_solution_givenDnaAndQuery_returnsAnswers(string gen, int[] P, int[] Q, int[] expected)
+        {
+            var target = new GenomicRangeQuery();
+            var actual = target.solution(gen, P, Q);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
