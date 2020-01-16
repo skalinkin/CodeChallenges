143cc59439c7e02d88eb136a1a311d9451304a49
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-15 20:14:17 -0500

    adding MaxCounters

---
+++ CodeChallengesTests/MaxCountersSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class MaxCountersSolutionTest
+    {
+        [TestCase(new[] {3, 4, 4, 6, 1, 4, 4}, 5, new[] {3, 2, 2, 4, 2})]
+        public void test_solution_givenArray_returnsCounters(int[] given, int counterNumber, object expected)
+        {
+            var target = new MaxCounters();
+            var actual = target.solution(counterNumber, given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
