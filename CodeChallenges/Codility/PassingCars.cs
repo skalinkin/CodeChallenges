75dddfef7f7fa52475186dcf046f1f544555b218
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-16 11:52:33 -0500

    Adding PassingCars solution

---
+++ CodeChallenges/Codility/PassingCars.cs
@@ -0,0 +1,37 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class PassingCars
+    {
+        public int solution(int[] A)
+        {
+            var pairs = 0;
+            var firstCar = A[0];
+            var multiplayer = 0;
+            var foundFirstCarTravelingEast = false;
+            foreach (var element in A)
+            {
+                if (element == 1 && !foundFirstCarTravelingEast)
+                {
+                    continue;
+                }
+
+                foundFirstCarTravelingEast = true;
+
+                if (element == firstCar)
+                {
+                    multiplayer++;
+                }
+                else
+                {
+                    pairs += multiplayer;
+                    if (pairs > 1000000000)
+                    {
+                        return -1;
+                    }
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
---
+++ CodeChallengesTests/PassingCarsSolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class PassingCarsSolutionTest
+    {
+        [TestCase(new []{0,1,0,1,1},5)]
+        [TestCase(new []{0,1},1)]
+        [TestCase(new []{0,0},0)]
+        [TestCase(new []{1,0},0)]
+        public void test_solution_givenNonEmptyArray_returnsTheNumberOfPairs(int[] given, int expected)
+        {
+            var target = new PassingCars();
+            int actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
