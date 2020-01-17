6a54a1a733d01b88e1d648355bdec925e9656e17
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-17 14:45:08 -0500

    Added CountDiv solution

---
+++ CodeChallengesTests/CountDivSolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class CountDivSolutionTest
+    {
+        [TestCase(6,11,2,3)]
+        [TestCase(6,11,12,0)]
+        public void test_solution_GivenRangeAndDevider_retunsNumberOfDivisibles(int A, int B, int K, int expected)
+        {
+            var target = new CountDiv();
+            var actual = target.solution(A,B,K);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
