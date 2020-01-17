71cfd0d85a019856e390dfff661dfbca544472dc
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-17 15:30:20 -0500

    Adding Distinct solution

---
+++ CodeChallengesTests/DistinctSolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class DistinctSolutionTest
+    {
+        [TestCase(new int[]{2,1,1,2,3,1},3)]
+        [TestCase(new int[]{2,1,5,4,3,6},6)]
+        [TestCase(new int[]{1},1)]
+        public void test_solution_givenArrayOfInterer_returnsNumberOfDistinctElements(int[] given, int expected)
+        {
+            var target = new Distinct();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
