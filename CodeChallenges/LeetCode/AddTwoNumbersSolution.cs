1cd0c5c6cf19c7c367f4a22a8cd3f43fe1b79d93
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-18 21:54:23 -0500

    Adding AddTwoNumbersSolution solution

---
+++ CodeChallenges/LeetCode/AddTwoNumbersSolution.cs
@@ -0,0 +1,41 @@
+﻿namespace CodeChallenges.LeetCode
+{
+    public class AddTwoNumbersSolution
+    {
+        private int cary;
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var sum = cary;
+            sum += l1?.val ?? 0;
+            sum += l2?.val ?? 0;
+            if (l1 == null && l2 == null)
+            {
+                if (sum > 0)
+                {
+                    return new ListNode(sum);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            cary = sum / 10;
+            var node = new ListNode(sum % 10);
+            node.next = AddTwoNumbers(l1?.next, l2?.next);
+            return node;
+        }
+    }
+
+    public class ListNode
+    {
+        public ListNode next;
+        public int val;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
---
+++ CodeChallengesTests/AddTwoNumbersSolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using CodeChallenges.LeetCode;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class AddTwoNumbersSolutionTest
+    {
+        [Test]
+        public void test_returnsSumLinkedList()
+        {
+            var l1 = new ListNode(2) {next = new ListNode(4) {next = new ListNode(3)}};
+            var l2 = new ListNode(5) {next = new ListNode(6) {next = new ListNode(4)}};
+
+            var target = new AddTwoNumbersSolution();
+            var actual = target.AddTwoNumbers(l1,l2);
+        }
+    }
+}
