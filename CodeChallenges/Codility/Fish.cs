1369bf5644072d6b1c5f6b85532890c7fadbcd3b
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-20 19:02:16 -0500

    Adding Fish solution

---
+++ CodeChallenges/Codility/Fish.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace CodeChallenges.Codility
+{
+    public class Fish
+    {
+        public int solution(int[] A, int[] B)
+        {
+            var alive = 0;
+            var N = A.Length;
+            var downstream = new Stack<int>(N);
+
+            for (var i = 0; i < N; i++)
+            {
+                if (B[i] == 0)
+                {
+                    while (downstream.Count > 0)
+                    {
+                        if (downstream.Peek() < A[i])
+                        {
+                            downstream.Pop();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (downstream.Count == 0)
+                    {
+                        alive++;
+                    }
+                }
+                else
+                {
+                    downstream.Push(A[i]);
+                }
+            }
+
+            alive += downstream.Count;
+            return alive;
+        }
+    }
+}
