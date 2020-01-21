2e0f542a117345239b71ad7496f76c38de18c8c7
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-21 13:59:57 -0500

    Adding Dominator solution

---
+++ CodeChallenges/Codility/Dominator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenges.Codility
+{
+    public class Dominator
+    {
+        public int solution(int[] A)
+        {
+            var counts = new Dictionary<int, int>();
+
+            foreach (var i in A)
+            {
+                if (!counts.ContainsKey(i))
+                {
+                    counts.Add(i, 1);
+                }
+                else
+                {
+                    counts[i]++;
+                }
+            }
+
+            if (counts.Count != 0)
+            {
+                var maxCount = counts.First().Value;
+                var leadNumber = counts.First().Key;
+
+                foreach (var count in counts)
+                {
+                    if (maxCount < count.Value)
+                    {
+                        maxCount = count.Value;
+                        leadNumber = count.Key;
+                    }
+                }
+
+                if (maxCount > A.Length / 2)
+                {
+                    return Array.IndexOf(A, leadNumber);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ CodeChallengesTests/DominatorSolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class DominatorSolutionTest
+    {
+        [TestCase(new[] {1, 3, 4, 3, 2, 3, -1, 3, 3}, new[] {1, 3, 5, 8})]
+        [TestCase(new[] {1, 1, 1, 1, 1, 2, 2, 2, 2, 2}, new[] {-1})]
+        [TestCase(new int[] { }, new[] {-1})]
+        [TestCase(new[] {1}, new[] {0})]
+        public void test_solution_givenArray_returnsDominator(int[] given, int[] expected)
+        {
+            var target = new Dominator();
+            var actual = target.solution(given);
+            CollectionAssert.Contains(expected, actual);
+        }
+    }
+}
