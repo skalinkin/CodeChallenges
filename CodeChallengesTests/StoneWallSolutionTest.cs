a703f25545611107ab14df2267a57590c68cfe1d
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-20 20:53:38 -0500

    Adding StoneWall Solution

---
+++ CodeChallengesTests/StoneWallSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class StoneWallSolutionTest
+    {
+        [TestCase(new[] {8, 8, 5, 7, 9, 8, 7, 4, 8}, 7)]
+        public void test_solution_givenHieghOfTheWall_returnsNumberOfBlocks(int[] given, int expected)
+        {
+            var target = new StoneWall();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
