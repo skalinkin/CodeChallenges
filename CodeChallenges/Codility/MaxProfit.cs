5ff661c9b2b933ab5361914a89741800b50b749a
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-23 08:37:07 -0500

    Adding MaxProfit solution

---
+++ CodeChallenges/Codility/MaxProfit.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class MaxProfit
+    {
+        public int solution(int[] A)
+        {
+            var profit = 0;
+            var min = int.MaxValue;
+
+            foreach (var price in A)
+            {
+                min = price < min ? price : min;
+                profit = price - min > profit ? price - min : profit;
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ CodeChallengesTests/MaxProfitSolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class MaxProfitSolutionTest
+    {
+        [TestCase(new[] {23171, 21011, 21123, 21366, 21013, 21367}, 356)]
+        [TestCase(new[] {1, 2}, 1)]
+        [TestCase(new[] {2, 1}, 0)]
+        [TestCase(new[] {8, 9, 3, 6, 1, 2}, 3)]
+        public void test_methodName_withCertainState_shouldDoSomething(int[] given, int expected)
+        {
+            var target = new MaxProfit();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
