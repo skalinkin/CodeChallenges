acbcfa6eaf21c1a3ba1886023e64a54252eaadbd
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-23 11:34:06 -0500

    Adding max slice sum solution

---
+++ CodeChallenges/Codility/MaxSliceSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeChallenges.Codility
+{
+    public class MaxSliceSum
+    {
+        public int solution(int[] A)
+        {
+            var N = A.Length;
+            if (N==1)
+            {
+                return A[0];
+            }
+
+            var maxSlice = A[0];
+            var maxCurrent = A[0];
+
+            for (var index = 1; index < A.Length; index++)
+            {
+                var number = A[index];
+                maxCurrent = Math.Max(A[index], maxCurrent + number);
+                maxSlice = Math.Max(maxSlice, maxCurrent);
+            }
+
+            return maxSlice;
+        }
+    }
+}
---
+++ CodeChallengesTests/MaxSliceSumSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests
+{
+    [TestFixture]
+    public class MaxSliceSumSolutionTest
+    {
+        [TestCase(new[] {3, 2, -6, 4, 0}, 5)]
+        public void test_solution_givenArray_returnsMaxSliceSum(int[] given, int expected)
+        {
+            var target = new MaxSliceSum();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
