f9edc91b00aa216a8575bfad8552bd2a8bd8bea8
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-24 15:54:14 -0500

    working on MaxDoubleSliceSum

---
+++ CodeChallenges/Codility/MaxDoubleSliceSum.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace CodeChallenges.Codility
+{
+    public class MaxDoubleSliceSum
+    {
+        public int solution(int[] A)
+        {
+            var N = A.Length;
+            var queue = new Queue<int>(N);
+
+            var curentSlice = A[0];
+            for (int i = 1; i < N -1 ; i++)
+            {
+                if (A[i] > A[i + 1])
+                {
+                    queue.Enqueue(curentSlice);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CodeChallengesTests/Codility/MaxDoubleSliceSumSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.Codility
+{
+    [TestFixture]
+    public class MaxDoubleSliceSumSolutionTest
+    {
+        [TestCase(new[] {3, 2, 6, -1, 4, 5, -1, 2}, 17)]
+        public void test_soltuion_givenAray_returnsMaxDoubleSliceSum(int[] given, int expected)
+        {
+            var target = new MaxDoubleSliceSum();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
