3d5ffd84bd729e6b353a39e6297ce52e1da18540
Sergiy Kalinkin <skalinkin@gmail.com>
2020-01-28 15:42:22 -0500

    Adding solution for MinPerimeterRectangle

---
+++ CodeChallenges/Codility/MinPerimeterRectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeChallenges.Codility
+{
+    public class MinPerimeterRectangle
+    {
+        public int solution(int N)
+        {
+            var minPerimeter = int.MaxValue;
+
+            if (N == 1)
+            {
+                return 4;
+            }
+
+            for (var i = 1; i < N / 2; i++)
+            {
+                if (N % i == 0)
+                {
+                    minPerimeter = Math.Min(minPerimeter, (N / i + i) * 2);
+                }
+            }
+
+            return minPerimeter;
+        }
+    }
+}
---
+++ CodeChallengesTests/Codility/MinPerimeterRectangleSolutionTest.cs
@@ -0,0 +1,21 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.Codility
+{
+    [TestFixture]
+    public class MinPerimeterRectangleSolutionTest
+    {
+        [TestCase(30,22)]
+        [TestCase(1,4)]
+        [TestCase(1000000000, 126500)]
+        [TestCase(101, 204)]
+        [TestCase(15486451, 30972904)]
+        public void test_solution_givenAnNumber_returnsMinimalPerimeter(int given, int expected)
+        {
+            var target = new MinPerimeterRectangle();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
