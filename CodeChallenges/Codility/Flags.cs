2d4cd6130165cf4df89ff12d1c318f076ecb5ff2
Sergiy Kalinkin <skalinkin@gmail.com>
2020-02-11 15:19:06 -0500

    starting Flags

---
+++ CodeChallenges/Codility/Flags.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class Flags
+    {
+        public int solution(int[] A)
+        {
+            var flags = 0;
+
+            return flags;
+        }
+    }
+}
---
+++ CodeChallengesTests/Codility/FlagsSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.Codility
+{
+    [TestFixture]
+    public class FlagsSolutionTest
+    {
+        [TestCase(new[] {1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}, 3)]
+        public void test_solution_givenMountenRange_returnsNumberOfFlags(int[] given, int expected)
+        {
+            var target = new Flags();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
