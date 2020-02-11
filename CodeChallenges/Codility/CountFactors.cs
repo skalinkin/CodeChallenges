c9d74e6f126b59be9da5d937d51c084187a069c4
Sergiy Kalinkin <skalinkin@gmail.com>
2020-02-11 14:58:44 -0500

    Adding solution to Count Factors problem

---
+++ CodeChallenges/Codility/CountFactors.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class CountFactors
+    {
+        public int solution(int N)
+        {
+            if (N == 1)
+            {
+                return 1;
+            }
+
+            var count = 2;
+            for (var i = 2; i <= N / 2; i++)
+            {
+                if (N % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ CodeChallengesTests/Codility/CountFactorsSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.Codility
+{
+    [TestFixture]
+    public class CountFactorsSolutionTest
+    {
+        [TestCase(24,8)]
+        public void test_solution_givenN_returnsNumberOfItsFactors(int given, int expected)
+        {
+            var target = new CountFactors();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
