a425495faf74d9c235451852c812cdfbcb0389f3
Sergiy Kalinkin <skalinkin@gmail.com>
2020-02-17 11:20:49 -0500

    Solution for Peaks

---
+++ CodeChallenges/Codility/Peaks.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace CodeChallenges.Codility
+{
+    public class Peaks
+    {
+        public int solution(int[] A)
+        {
+            var peaks = new List<int>();
+
+            for (var i = 1; i < A.Length - 1; i++)
+            {
+                if (A[i - 1] < A[i] && A[i] > A[i + 1])
+                {
+                    peaks.Add(i);
+                }
+            }
+
+            if (peaks.Count == 0)
+            {
+                return 0;
+            }
+
+
+            for (int i = peaks.Count; i > 0; i--)
+            {
+                if (A.Length % i == 0)
+                {
+                    var blockSize = A.Length / i;
+                    var found = new bool [i];
+                    var foundCount = 0;
+
+                    foreach (var peak in peaks)
+                    {
+                        var blockNr = peak / blockSize;
+                        if (!found[blockNr])
+                        {
+                            found[blockNr] = true;
+                            foundCount++;
+                        }
+                    }
+
+                    if (foundCount == i)
+                    {
+                        return i;
+                    }
+
+                } 
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CodeChallengesTests/Codility/PeaksSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.Codility
+{
+    [TestFixture]
+    public class PeaksSolutionTest
+    {
+        [TestCase(new []{1,2,3,4,3,4,1,2,3,4,6,2}, 3)]
+        public void test_methodName_withCertainState_shouldDoSomething(int[] given, int expected)
+        {
+            var target = new Peaks();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
