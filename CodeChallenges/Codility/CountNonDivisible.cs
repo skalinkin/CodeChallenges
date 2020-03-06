11a824c4d3f25c0437e5944bbff4461f4fc34f95
Sergiy Kalinkin <skalinkin@gmail.com>
2020-03-06 08:31:54 -0500

    Adding Naive solution to CountNonDivisible problem

---
+++ CodeChallenges/Codility/CountNonDivisible.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeChallenges.Codility
+{
+    public class CountNonDivisible
+    {
+        public int[] solution(int[] A)
+        {
+            var N = A.Length;
+            var result = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i != j)
+                    {
+                        if (A[i] % A[j] != 0)
+                        {
+                            result[i]++;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CodeChallengesTests/Codility/CountNonDivisibleSolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Codility;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.Codility
+{
+    [TestFixture]
+    public class CountNonDivisibleSolutionTest
+    {
+        [TestCase(new[] {3, 1, 2, 3, 6}, new[] {2, 4, 3, 2, 0})]
+        public void test_solution_givenArray_returnsAmountSequence(int[] given, int[] expected)
+        {
+            var target = new CountNonDivisible();
+            var actual = target.solution(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
