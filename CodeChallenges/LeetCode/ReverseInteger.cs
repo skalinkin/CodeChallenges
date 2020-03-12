c3a69e382d390403f169400b5302439609ef1100
Sergiy Kalinkin <skalinkin@gmail.com>
2020-03-12 10:53:58 -0400

    solution for Revers Integer probelm on Leet Code

---
+++ CodeChallenges/LeetCode/ReverseInteger.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeChallenges.LeetCode
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var result = 0;
+            try
+            {
+                var remainder = 0;
+                while (x != 0 || remainder != 0)
+                {
+                    checked
+                    {
+                        if (x != 0)
+                        {
+                            result *= 10;
+                        }
+
+                        remainder = x % 10;
+                        result += remainder;
+                        x /= 10;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CodeChallengesTests/LeetCode/ReverseIntegerSolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenges.LeetCode;
+using NUnit.Framework;
+
+namespace CodeChallengesTests.LeetCode
+{
+    [TestFixture]
+    public class ReverseIntegerSolutionTest
+    {
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(1534236469, 0)]
+        public void test_methodName_withCertainState_shouldDoSomething(int given, int expected)
+        {
+            var target = new ReverseInteger();
+            var actual = target.Reverse(given);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
